4db87ccf754add89a9961b47fc3948c7946f6f71
mormareima <76215591+mormareima@users.noreply.github.com>
2021-01-31 12:13:47 -0500

    archivos gpio cuda
    tesgpio.sh es para  el archivo gpiocu.cu y el otro testgpio2.sh es para el gpiocuda1.cpp

---
+++ CUDA/testgpio.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+/usr/local/cuda/bin/nvcc gpiocu.cu -o test3
+./test1
+#exit
+
+
---
+++ CUDA/testgpio2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+/usr/local/cuda/bin/nvcc gpiocuda1.cu -o test2
+./test1
+#exit
+
+
