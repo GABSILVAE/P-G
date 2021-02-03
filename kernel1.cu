23806cb7e704aad6cd5840728dda7db84bdf65d5
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-03 09:09:45 -0500

    Update kernel1.cu

--- kernel1.cu
+++ kernel1.cu
@@ -16,6 +16,24 @@ using namespace std;
 #define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
 #define MAX_BUF 64
 
+__host__ int gpio_export(unsigned int gpio){
+	int fd, len;
+	char buf[MAX_BUF];
+ 
+	fd = open(SYSFS_GPIO_DIR "/export", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+ 
+	return 0;
+}
+
+
 __host__ void writeFile(char *fileName, char line[]) {
 	FILE* file;
 	file = fopen(fileName, "w");
