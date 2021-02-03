a03d1c16062ea7e9b855919ee0ade4f3b88003d1
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-03 08:52:20 -0500

    Add files via upload

---
+++ kernel1.cu
@@ -0,0 +1,32 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <cuda_runtime.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+
+using namespace std;
+
+__host__ void writeFile(char *fileName, char line[]) {
+	FILE* file;
+	file = fopen(fileName, "w");
+
+	fputs(line, file);
+
+	fclose(file);
+}
+
+int main(int *argc, char** argv[]) {
+	printf("Script for writting a line to a txt file");
+
+	char line[255] = "Helo world from Host";
+
+	writeFile("test.txt", line);
+
+	return 0;
+}
