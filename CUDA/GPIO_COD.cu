c291ebf40d42c30b3bfd384bd62b5ee2674c63ca
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-08 17:03:30 -0500

    se quito la linea #define POLL_TIMEOUT (3 * 1000) /* 3 seconds */

--- CUDA/GPIO_COD.cu
+++ CUDA/GPIO_COD.cu
@@ -14,7 +14,6 @@
 using namespace std;
 
 #define SYSFS_GPIO_DIR "/sys/class/gpio"
-#define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
 #define MAX_BUF 64
 
 unsigned int Key_deco[41]={0,0,0,0,0,0,0,216,0,0,0,50,79,14,0,194,232,0,15,16,0,17,13,18,19,0,20,0,0,149,0,200,168,38,0,76,51,12,77,0,78};
