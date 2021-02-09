bbbb75b0dff5cb5bb3ef56926e943b3fef11dd72
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-08 19:03:58 -0500

    .

--- CUDA/LIBGPIO/GPIO_our.cu
+++ CUDA/LIBGPIO/GPIO_our.cu
@@ -1,4 +1,36 @@
-#include "GPIO_our.cuh"
+#ifndef GPIO_our_h
+#define GPIO_our_h
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include <chrono> 
+#include <thread>
+#include <cuda_runtime.h>
+#define SYSFS_GPIO_DIR "/sys/class/gpio"
+#define MAX_BUF 64
+
+__host__ int gpio_export(unsigned int gpio);
+__host__ int gpio_unexport(unsigned int gpio);
+__host__ int gpio_set_dir(unsigned int gpio, unsigned int out_flag);
+__host__ int gpio_set_value(unsigned int gpio, unsigned int value);
+__host__ int gpio_get_value(unsigned int gpio, unsigned int *value);
+__host__ int gpio_set_edge(unsigned int gpio, char *edge);
+__host__ int gpio_fd_open(unsigned int gpio);
+__host__ int gpio_fd_close(int fd);
+__host__ void delay(int s);}
+
+#endif GPIO_our_h
+
+
+#ifndef GPIO_our_cu
+#define GPIO_our_cu
+
 using namespace std;
 
 __host__ int gpio_export(unsigned int gpio){
@@ -138,3 +170,5 @@ __host__ int gpio_fd_close(int fd){
 __host__ inline void delay(int s){
 	this_thread::sleep_for(chrono::seconds(s));
 }
+
+#endif GPIO_our_cu
