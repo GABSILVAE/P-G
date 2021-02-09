388c109e17c7da984872331395e110166b2e2d33
mormareima <76215591+mormareima@users.noreply.github.com>
2021-02-09 17:37:34 -0500

    Create GPIOin.cu
    .

---
+++ LIBRERIAS/LIBGPIO/GPIOin.cu
@@ -0,0 +1,36 @@
+#include "GPIOlib.cu"
+
+using namespace std;
+unsigned int in,out,pin_in,pin_out,valor;
+unsigned int Key_deco[41]={0,0,0,0,0,0,0,216,0,0,0,50,79,14,0,194,232,0,15,16,0,17,13,18,19,0,20,0,0,149,0,200,168,38,0,76,51,12,77,0,78};
+
+int main(int *argc,char**argv[]){
+	
+	pin_in=13;
+	pin_out=7;
+	in=Key_deco[pin_in];
+	out = Key_deco[pin_out];
+	
+	gpio_export(in);
+	delay(1);	
+	gpio_export(out);
+	delay(1);
+	
+	gpio_dir(in,0);
+	delay(1);
+	gpio_dir(out,1);
+	delay(1);
+	while(1){
+		
+		gpio_get_value(in,valor);
+		
+		gpio_set_value(out,value);
+		if(valor==1){
+			cout << "switch encendido"<<"\n"
+		}
+		else{
+			cout << "switch apagado"<<"\n";
+		}
+	}
+	return 0;
+}
