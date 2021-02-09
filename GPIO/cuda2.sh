00d74e23da4da615d4c4854600f3156129bf6db5
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-08 19:36:21 -0500

    .

--- GPIO/cuda2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-/usr/local/cuda/bin/nvcc simple_out.cpp -o test1
-./test1
-#exit
-
-
--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-/usr/local/cuda/bin/nvcc kernel1.cu -o test1
-./test1
-#exit
