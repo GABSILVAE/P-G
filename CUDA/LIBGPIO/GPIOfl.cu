1d305e91d14c928070d09ed74b9f7b726ff5df7a
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-11 18:33:32 -0500

    .

--- CUDA/LIBGPIO/GPIOfl.cu
+++ CUDA/LIBGPIO/GPIOfl.cu
@@ -1,4 +1,6 @@
 #include "GPIOlib.cu"
+#define POLL_TIMEOUT (3 * 1000) /* 3 seconds */
+
 
 using namespace std;
 unsigned int Key_deco[41]={0,0,0,0,0,0,0,216,0,0,0,50,79,14,0,194,232,0,15,16,0,17,13,18,19,0,20,0,0,149,0,200,168,38,0,76,51,12,77,0,78};
