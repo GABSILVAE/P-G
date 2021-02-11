0296222c55c78e09af89e1986d13f2f95d4f3575
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-11 18:56:47 -0500

    .

--- CUDA/LIBGPIO/1.cu
+++ CUDA/LIBGPIO/1.cu
@@ -246,20 +246,9 @@ int main(int argc, char **argv, char **envp)
       
 		fdset[1].fd = gpio_fd;
 		fdset[1].events = POLLPRI;
-
-		rc = poll(fdset, nfds, timeout);      
-
-		if (rc < 0) {
-			printf("\npoll() failed!\n");
-			return -1;
-		}
-      
-		if (rc == 0) {
-			printf(".");
-		}
             
 		if (fdset[1].revents & POLLPRI) {
-			
+			lseek(fdset[1].fd, 0, SEEK_SET);
 			len = read(fdset[1].fd, buf, MAX_BUF);
 			printf("\npoll() GPIO %d interrupt occurred\n", gpio);
 		}
