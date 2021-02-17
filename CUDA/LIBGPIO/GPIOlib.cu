afc02c2dd7d5366d115503a17c2af19a9db945ea
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-17 07:14:29 -0500

    .

--- CUDA/LIBGPIO/GPIOlib.cu
+++ CUDA/LIBGPIO/GPIOlib.cu
@@ -10,8 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
-//#include <unistd.h>
-#include <fcntl.h>
+#include <unistd.h>
+//#include <fcntl.h>
 //#include <poll.h>
 //#include <chrono> 
 #include <thread>					--lib Delay 
