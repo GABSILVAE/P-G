5713b588351e9d238e1c5ac03b26a306d065fb05
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-17 07:22:26 -0500

    .

--- CUDA/LIBGPIO/GPIOlib.cu
+++ CUDA/LIBGPIO/GPIOlib.cu
@@ -5,15 +5,11 @@
 #ifndef GPIOlib_h
 #define GPIOlib_h
 
-#include <iostream>
+//#include <iostream>
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
 #include <errno.h>					//Informes de error
 #include <unistd.h>
 #include <fcntl.h>
-//#include <poll.h>
-//#include <chrono> 
 #include <thread>					//lib Delay 
 #include <cuda_runtime.h>			//lib Delay
 
