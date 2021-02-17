8d9fe0067af2d475befebefd33ac28282727c91d
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-17 14:33:30 -0500

    .

--- GPIO/1.cu
+++ GPIO/1.cu
@@ -1,16 +1,25 @@
+#include <linux/module.h>   
+#include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/gpio.h>
-#include <iostream>
+#include <linux/interrupt.h>
+
+#define LED 27
+#define BUTTON 23
+#define MY_GPIO_INT_NAME "my_button_int"
+#define MY_DEV_NAME "my_device"
 
 int main(int *argc, char** argv[]){	
-    const bool is17Valid = !!gpio_is_valid(17);
-    if (is17Valid)
-    {
-        std::cout << "17 is valid!" << std::endl;
-    }
-    else
-    {
-        std::cout << "17 is *not* valid!" << std::endl;
-    }
+	// -- setup the led gpio as output
+	printk("module: button interrupt example.\n");
+	if(gpio_is_valid(LED) < 0) return -1;
+	if(gpio_request(LED, "LED") < 0) return -1;
+	gpio_direction_output(LED, 0 );
 
-    return 0;
+	// -- setup the button gpio as input and request irq
+	if(gpio_request(BUTTON,"BUTTON") < 0) return -1;
+	if(gpio_is_valid(BUTTON) < 0) return -1;
+	if( (button_irq = gpio_to_irq(BUTTON)) < 0 )  return -1;
+	if( request_irq( button_irq, button_isr ,IRQF_TRIGGER_RISING, MY_GPIO_INT_NAME, MY_DEV_NAME)) return -1;
+	return 0;
 }
