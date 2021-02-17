c3c8ce42ed08092f5d25b2ad901ba3e7a3dfa064
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-02-17 14:10:00 -0500

    .

--- GPIO/1.cu
+++ GPIO/1.cu
@@ -1,5 +1,16 @@
 #include <linux/gpio.h>
+#include <iostream>
 
 int main(int *argc, char** argv[]){	
-	return 0;
+    const bool is17Valid = !!gpio_is_valid(17);
+    if (is17Valid)
+    {
+        std::cout << "17 is valid!" << std::endl;
+    }
+    else
+    {
+        std::cout << "17 is *not* valid!" << std::endl;
+    }
+
+    return 0;
 }
