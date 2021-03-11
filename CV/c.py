3dee4739e5ecd3ebd712f16a39aa3eedab84a6fa
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-03-10 20:06:00 -0500

    .

--- CV/c.py
+++ CV/c.py
@@ -1,2 +1,14 @@
-import timeit
-print(timeit.timeit('output = 10*5'))
+import numpy as np
+import cv2 as cv
+
+img = cv2.imread("figContorno.png", cv2.IMREAD_GRAYSCALE)
+src = cv2.cuda_GpuMat()
+src.upload(img)
+ 
+clahe = cv2.cuda.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
+dst = clahe.apply(src, cv2.cuda_Stream.Null())
+ 
+result = dst.download()
+ 
+cv2.imshow("result", result)
+cv2.waitKey(0)
