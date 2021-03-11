bb21c5609aca164d904f94f1ed7ab9b104280762
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-03-10 20:02:07 -0500

    .

--- CV/b.py
+++ CV/b.py
@@ -6,4 +6,4 @@ npTmp = np.random.random((1024, 1024)).astype(np.float32)
 npMat1 = npMat2 = npMat3 = npDst = np.stack([npTmp,npTmp],axis=2)
 cuMat1 = cuMat2 = cuMat3 = cuDst = cv.cuda_GpuMat(npMat1)
 
-print(timeit cv2.cuda.gemm(cuMat1, cuMat2,1,cuMat3,1,cuDst,1))
+print(timeit.timeit(cv.cuda.gemm(cuMat1, cuMat2,1,cuMat3,1,cuDst,1)))
