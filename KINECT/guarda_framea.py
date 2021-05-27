879182e310f71e9ada5b6f25fe8258b393ef931a
GABSILVAE <gabriel.silva@uptc.edu.co>
2021-05-27 16:21:28 -0500

    .

--- KINECT/guarda_framea.py
+++ KINECT/guarda_framea.py
@@ -8,20 +8,22 @@ import numpy as np
 depth = get_depth()[0]
 
 output = depth.astype(np.uint8)
-print(output.shape)
+print(output)
 cv2.imshow('Depth', output)
 
 cv2.imwrite('Capture.jpg', output)
 
 img = cv2.imread('Capture.jpg')
 print('################################################################################################')
-print(img.shape)
+
  
 
 blur = cv2.blur(img,(3,3))
 
-mat=pd.DataFrame(data=output)
-mat.to_csv('kinect_valores_300cmg2_csv',sep=' ',header=False,float_format='%d',index=False)
+mat = pd.DataFrame(data=output)
+mat.to_csv('asd.csv',sep=',',header=False,float_format='%d',index=False)
+
+
 
 plt.subplot(121),plt.imshow(img),plt.title('Original')
 plt.xticks([]), plt.yticks([])
