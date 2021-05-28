4785c3f807e30aebf2ca554156111aa568916a29
GABSILVAE <gabriel.silva@uptc.edu.co>
2021-05-28 15:28:09 -0500

    funcion ubralización

--- pycuda/umbral.py
+++ pycuda/umbral.py
@@ -14,7 +14,7 @@ from PIL import Image
 
 
 kernel_code_template = """
-__global__ void MatrixMulKernel(float *rgbimage, float *grayimage, float *binimage)
+__global__ void MatrixMulKernel( float *grayimage, float *binimage)
 {
     int tx = threadIdx.x + blockDim.x * blockIdx.x;
     int ty = threadIdx.y + blockDim.y * blockIdx.y;
@@ -22,26 +22,17 @@ __global__ void MatrixMulKernel(float *rgbimage, float *grayimage, float *binima
     
     if(tx<%(columna)s && ty< %(fila)s){
 
-        int grayOffset = %(columna)s * ty + tx;
+       
+        int offsetgray = ty * %(columna)s + tx;
 
-        int rgbOffset = grayOffset * %(channels)s;
+        if(grayimage[offsetgray]<%(umbral)s){
 
-        unsigned int r = rgbimage[rgbOffset];
-
-        unsigned int g = rgbimage[rgbOffset +1];
-
-        unsigned int b = rgbimage[rgbOffset +2];
-
-        grayimage[grayOffset] = ((r + g + b)/3);
-
-        if(grayimage[grayOffset]<%(umbral)s){
-
-            binimage[grayOffset]=0;
+            binimage[offsetgray]=0;
         }
 
         else{
 
-            binimage[grayOffset]=1;
+            binimage[offsetgray]=1;
         }
     }
   
@@ -52,30 +43,26 @@ __global__ void MatrixMulKernel(float *rgbimage, float *grayimage, float *binima
 
 
 
-image=img.imread("shingeki.jpg")
+image=img.imread("shingeki_gray.jpeg")
 print(image.shape)
 r_img= image[:,:,0]
 
 
-fila,columna,canales = image.shape
-image_array_list=[]
+fila,columna = r_img.shape
 
-for row in range(fila):
-    for column in range(columna):
-        for channel in range (canales):
-            image_array_list.append(image[row][column][channel])
 
-image_list=np.array(image_array_list)
+
+image_list=np.array(r_img)
 
 print(image_list.shape)
 
 image_rgb= np.array(image_list). astype(np.float32)
-image_gray=np.zeros((fila,columna)). astype(np.float32)
+
 image_bin=np.zeros((fila,columna)). astype(np.float32)
 
 
-rgb_gpu = cuda.mem_alloc(image_rgb.nbytes)
-gray_gpu = cuda.mem_alloc(image_gray.nbytes)
+r_gpu = cuda.mem_alloc(image_rgb.nbytes)
+
 bin_gpu = cuda.mem_alloc(image_bin.nbytes)
 
 umbral=150
@@ -83,7 +70,6 @@ umbral=150
 kernel_code = kernel_code_template % {
     'fila': fila, 
     'columna': columna,
-    'channels':canales,
     'umbral': umbral
     }
 
@@ -92,13 +78,12 @@ mod = compiler.SourceModule(kernel_code)
 matrixmul = mod.get_function("MatrixMulKernel")
 
 
-cuda.memcpy_htod(rgb_gpu, image_rgb)
+cuda.memcpy_htod(r_gpu, image_rgb)
 
 matrixmul(
     # inputs
-    rgb_gpu,  
+    r_gpu,  
     #output
-    gray_gpu,
     bin_gpu,
     # (only one) block of MATRIX_SIZE x MATRIX_SIZE threads
     block = (6,36, 1),
@@ -110,7 +95,6 @@ gr= np.zeros((fila, columna)). astype(np.float32)
 bn= np.zeros((fila, columna)). astype(np.float32)
 
 
-cuda.memcpy_dtoh(gr, gray_gpu)
 
 cuda.memcpy_dtoh(bn, bin_gpu)
 bn= bn.astype(np.uint8)
@@ -118,7 +102,7 @@ bn= bn.astype(np.uint8)
 print(kernel_code)
 print(bn.shape)
 
-image = Image.fromarray(bn).save('pic1.png')
+#image = Image.fromarray(bn).save('pic1.png')
 '''umbral = image.convert("L")
 
 base_dir = os.getcwd()
