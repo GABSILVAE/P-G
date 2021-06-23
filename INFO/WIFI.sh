ca8979a72752b4bd75f5f23c4e14ba1351484ee6
GABSILVAE <76257708+GABSILVAE@users.noreply.github.com>
2021-06-23 10:52:39 -0500

    .

---
+++ INFO/WIFI.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install git
+git clone https://github.com/jeremyb31/rtl8192eu-linux-driver
+cd rtl8192eu-linux-driver
+export ARCH=arm64
+make
+sudo make install
+
+echo "blacklist rtl8xxxu" | sudo tee /etc/modprobe.d/rtl8xxxu.conf;
+echo -e "8192eu\n\nloop" | sudo tee /etc/modules;
+sudo update-grub; sudo update-initramfs -u;
+systemctl reboot -i;
